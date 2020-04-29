b81154bce3b3c4790d7d54d30443bf34a9ce22ee
KhaledHMosaad <khaled.m.hisham@gmail.com>
2020-04-30 01:21:43 +0200

    initial setup

---
+++ ayat/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URI']
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+db = SQLAlchemy(app)
+
+import ayat.models
+
+
+
