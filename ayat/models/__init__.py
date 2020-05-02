72a0da9eb20d39b97d364355bdabb4d5bba840eb
KhaledHMosaad <khaled.m.hisham@gmail.com>
2020-05-02 20:01:38 +0200

    Refactoring Initial Setup

---
+++ ayat/models/__init__.py
@@ -0,0 +1,10 @@
+from ayat import app
+
+
+from ayat.models.users import *
+from ayat.models.assessments import *
+from ayat.models.programs import *
+from ayat.models.recitations import *
+from ayat.models.scheduling import *
+
+
